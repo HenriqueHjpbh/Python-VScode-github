0fdf16b66b3183d45e7d5489578161d812063124
HenriqueHjpbh <henrique_jpbh@hotmail.com>
2023-03-01 17:54:35 -0300

    cs50_python

---
+++ python-cs50-func.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("what's your name ?")
+    fuck_you(name)
+
+
+def fuck_you(to="world"):
+    print('fuck you,',to)
+
+
+main()
---
+++ python-cs50.py
@@ -0,0 +1,5 @@
+print('hello world')
+name = input('what\'s your name? ').strip().capitalize()
+print(f'Fuck you! {name}')
+print()
+#primeira fase cs50 backslash \ coloca como excessão ou literalidade os proximos argunentos 
