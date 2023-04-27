8e57996ab9b29413c23060f3e578cc5f487924b1
Henrique Pereira <80718496+HenriqueHjpbh@users.noreply.github.com>
2023-04-27 17:29:18 -0300

    Update python-cs50-func.py

--- python-cs50-func.py
+++ python-cs50-func.py
@@ -6,6 +6,8 @@ def main():
 
 def fuck_you(to="world"):
     print('fuck you,',to)
+    
+    
 #__name__ == '__main__' garante que não haja erros de execução se o programa chamar main() na hora errada
 if __name__ == "__main__":
     main()
